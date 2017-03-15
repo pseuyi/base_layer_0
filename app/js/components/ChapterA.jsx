271bdb72ccfb11450d3aeba0d179ea79f22fef6e
freda nada <fredasnada@gmail.com>
2017-03-15 13:21:38 -0400

    add placeholders, sections

--- app/js/components/ChapterA.jsx
+++ app/js/components/ChapterA.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const ChapterA = ()=>(
-  <div>
-    <div id='chapter-a' className="row">
-      <div className="col-md-8 big"><div className="col-content">.col-md-8</div></div>
-      <div className="col-md-4 big"><div className="col-content">.col-md-4</div></div>
-    </div>
+  <div id="chapter-a" className="row">
+    <h1>a) section_aaaaa</h1>
+    <div className="col-md-8 big"><div className="col-content">
+      <img src="/images/2.jpg" alt="silk" />
+    </div></div>
+    <div className="col-md-4 big"><div className="col-content">
+      <img src="/images/1.jpg" alt="silk" />
+    </div></div>
   </div>
 )
 
--- app/js/components/ChapterC.jsx
+++ app/js/components/ChapterC.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const ChapterC = ()=>(
   <div id="chapter-c" className="row">
-    <div className="col-md-8 big"><div className="col-content">.col-md-8</div></div>
-    <div className="col-md-4 big"><div className="col-content">.col-md-4</div></div>
+    <h1>c) section_Cc</h1>
+    <div className="col-md-2 big"><div className="col-content">
+      <img src="/images/1.jpg" alt="silk" />
+    </div></div>
+    <div className="col-md-8 big"><div className="col-content">
+      <img src="/images/5.jpg" alt="silk" />
+    </div></div>
+    <div className="col-md-2 big"><div className="col-content">
+      <img src="/images/1.jpg" alt="silk" />
+    </div></div>
   </div>
 )
 
